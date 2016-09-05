a9f1767cd69bf37c0efc00b9c1d3d6086b47cf81
JeongHyunLeeInnocent <wjdgus6163@gmail.com>
2016-09-05 11:41:54 +0900

    11 : 40

--- app/src/main/java/com/onemeter/omm/onemm/AgreeActivity.java
+++ app/src/main/java/com/onemeter/omm/onemm/AgreeActivity.java
@@ -6,14 +6,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,11 +25,13 @@ public class AgreeActivity extends AppCompatActivity {
     @BindView(R.id.text_box2)
     TextView editInfo;
     @BindView(R.id.check_use)
-    CheckBox checkUse;
+    ImageView checkUse;
     @BindView(R.id.check_info)
-    CheckBox checkInfo;
+    ImageView checkInfo;
 
     Toolbar toolbar;
+    Boolean info = false;
+    Boolean use = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,23 +41,37 @@ public class AgreeActivity extends AppCompatActivity {
 
         editInfo.setMovementMethod(new ScrollingMovementMethod());
         editUse.setMovementMethod(new ScrollingMovementMethod());
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         checkInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!info) {
+                    checkInfo.setImageResource(R.drawable.ic_checkbox_on);
+                    info = true;
+                } else  {
+                    checkInfo.setImageResource(R.drawable.ic_checkbox_off);
+                    info = false;
+                }
+                agreeCheck();
+            }
+        });
+        checkUse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!use) {
+                    checkUse.setImageResource(R.drawable.ic_checkbox_on);
+                    use = true;
+                } else  {
+                    checkUse.setImageResource(R.drawable.ic_checkbox_off);
+                    use = false;
+                }
                 agreeCheck();
             }
         });
 
-        checkUse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                agreeCheck();
-            }
-        });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,30 +94,8 @@ public class AgreeActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_item_1:
-                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.submenu_item_1:
-                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.submenu_item_2:
-                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     public boolean agreeCheck() {
-        if (checkUse.isChecked() && checkInfo.isChecked()) {
+        if (use && info) {
             agree.setBackgroundColor(Color.parseColor("#f82040"));
             return true;
 
