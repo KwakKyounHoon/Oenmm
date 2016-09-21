e93ab24962dde9274127512b49ebac8a2c1f831c
JeongHyunLeeInnocent <wjdgus6163@gmail.com>
2016-09-21 10:25:27 +0900

    21312312aa1111ll

--- app/src/main/java/com/onemeter/omm/onemm/SettingWithdrawDial.java
+++ app/src/main/java/com/onemeter/omm/onemm/SettingWithdrawDial.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Tacademy on 2016-09-13.
@@ -18,7 +19,7 @@ public class SettingWithdrawDial extends DialogFragment {
     String money;
     Button btn;
     TextView pointView;
-    TextView priceView;
+    TextView price;
 
     private static String MONEY = "money";
 
@@ -46,78 +47,68 @@ public class SettingWithdrawDial extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View view = getActivity().getLayoutInflater().inflate(R.layout.view_setting_withdraw, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(view);
 
         pointView = (TextView) view.findViewById(R.id.text_money);
-//        priceView = (TextView)view.findViewById(R.id.text_price);
-//
-//        String finalPoint = (String) priceView.getText();
-        if (Integer.parseInt(money) > 10000)
+
+        if (Integer.parseInt(money) >= 10000)
             pointView.setText(money + "원");
         else
             pointView.setText("보유금 부족");
 
-
-
         pointView.setTextColor(Color.parseColor("#FA8072"));
 
+        price = (TextView) view.findViewById(R.id.text_price);
+        btn = (Button) view.findViewById(R.id.btn_1000);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPrice += 1000;
+                price.setText("" + currentPrice);
+            }
+        });
 
-//
-//        builder.setTitle(null).setPositiveButton("확인", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialogInterface, int i) {
-//                String finalPoint = (String) price.getText();
-//                if (Integer.parseInt(finalPoint) >= 10000 && Integer.parseInt(money) >= Integer.parseInt(finalPoint)) {
-//                    Toast.makeText(getContext(), " 출금이 완료되었습니다 ", Toast.LENGTH_SHORT).show();
-//                    // 출금시 현재 보유금 차감
-//                } else {
-//                    Toast.makeText(getContext(), " 출금이 불가능 합니다", Toast.LENGTH_SHORT).show();
-//
-//                }
-//            }
-//        });
-//        if (Integer.parseInt(money) >= 10000)
-//            builder.setMessage("출금 가능액 : " + money + "원");
-//        else
-//            builder.setMessage("출금은 만원이상 가능합니다.");
-//
-//        price = (TextView) view.findViewById(R.id.text_price);
-//        btn = (Button) view.findViewById(R.id.btn_1000);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                currentPrice += 1000;
-//                price.setText("" + currentPrice);
-//            }
-//        });
-//
-//        btn = (Button) view.findViewById(R.id.btn_5000);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                currentPrice += 5000;
-//                price.setText("" + currentPrice);
-//            }
-//        });
-//
-//        btn = (Button) view.findViewById(R.id.btn_10000);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                currentPrice += 10000;
-//                price.setText("" + currentPrice);
-//            }
-//        });
-//
-//        btn = (Button) view.findViewById(R.id.btn_reset);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                currentPrice = 0;
-//                price.setText("" + currentPrice);
-//            }
-//        });
+        btn = (Button) view.findViewById(R.id.btn_5000);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPrice += 5000;
+                price.setText("" + currentPrice);
+            }
+        });
+
+        btn = (Button) view.findViewById(R.id.btn_10000);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPrice += 10000;
+                price.setText("" + currentPrice);
+            }
+        });
+
+        btn = (Button) view.findViewById(R.id.btn_reset);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPrice = 0;
+                price.setText("" + currentPrice);
+            }
+        });
+
+        btn = (Button) view.findViewById(R.id.btn_ok);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String finalPoint = (String) price.getText();
+                if (Integer.parseInt(finalPoint) >= 10000 && Integer.parseInt(money) >= Integer.parseInt(finalPoint)) {
+                    Toast.makeText(getContext(), " 출금이 완료 되었습니다. ", Toast.LENGTH_SHORT).show();
+                    dismiss();
+                } else {
+                    Toast.makeText(getContext(), " 금액이 부족합니다. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         return builder.create();
     }
 
